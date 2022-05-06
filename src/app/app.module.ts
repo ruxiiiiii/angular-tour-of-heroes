07ba1864f2b33283b276e5ce93226acf8357a1e9
Ruxandra Stanciu <rms995@student.bham.ac.uk>
2022-05-06 12:06:50 +0100

    finished editable hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+// Every component must be declared in exactly one NgModule.
+
 @NgModule({
   declarations: [AppComponent, HeroesComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
